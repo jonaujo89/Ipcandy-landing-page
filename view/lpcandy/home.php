5d49b7e8458e3c0d6051f2c36b3e569415f830ca
jonaujo89 <jonaujo89@gmail.com>
2015-01-16 15:16:39 +0400

    normal adaptive css start page lpcandy

--- view/lpcandy/home.php
+++ view/lpcandy/home.php
@@ -6,6 +6,26 @@
             font-size:12px;
             line-height:1.5em;
         }
+        @media (max-width:1369px) {
+            .container {
+              width: 930px;
+              margin: 0 auto;
+              margin-left: auto !important;
+              margin-right: auto !important;
+            }
+            .span16 {
+              display: inline;
+              float: left;
+              width: 930px;
+              margin-left: 30px;
+            }
+            .span16:first-child {
+              margin-left: 0;
+            }
+            .workOrder1 .item_list .item .name, .benefits_1 .item_list .item .name {
+              font-size: 18px;
+            }
+        }
     </style>
     <script src="<?=$assets.'/default.js'?>"></script>
     <?= $body_html ?>
