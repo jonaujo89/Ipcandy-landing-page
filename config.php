fabe8ee1c1c217b6c0f62a10de1958c361d992d2
jonaujo89 <jonaujo89@gmail.com>
2017-01-19 15:24:26 +0400

    'modified project_editors_group (users which see projects component)'

--- config.php
+++ config.php
@@ -8,7 +8,7 @@ $config = array(
     ],
     'acl' => [
         'project_editor_group' => [
-            'users' => [1,2,11,80,55, 158],
+            'users' => [1,2,11,80,55, 158, 156],
             'res' => ['project_editor']
         ]
     ],
