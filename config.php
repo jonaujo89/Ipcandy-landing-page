3fbc15be1c96a1a78812408024aa24deaea000ad
jonaujo89 <jonaujo89@gmail.com>
2019-08-02 00:11:56 +0200

    'allow Anna Arbuzova (id=214) to see cases component'

--- config.php
+++ config.php
@@ -8,7 +8,7 @@ $config = array(
     ],
     'acl' => [
         'project_editor_group' => [
-            'users' => [1,2,4,11,80,55, 158, 156],
+            'users' => [1,2,4,11,80,55, 158, 156, 214],
             'res' => ['project_editor']
         ]
     ],
