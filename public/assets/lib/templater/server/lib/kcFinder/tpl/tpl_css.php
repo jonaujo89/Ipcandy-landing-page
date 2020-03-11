160e5e9f0fc211c30581b03f43eafe49803472d5
jonaujo89 <jonaujo89@gmail.com>
2020-03-11 23:15:19 +0100

    'remove templater dep'

--- public/assets/lib/templater/server/lib/kcFinder/tpl/tpl_css.php
+++
@@ -1,2 +0,0 @@
-<link href="<?=KC_ROOT?>/css.php?type=<?php echo $this->type ?>" rel="stylesheet" type="text/css" />
-<link href="<?=KC_ROOT?>/themes/<?php echo $this->config['theme'] ?>/style.css" rel="stylesheet" type="text/css" />
