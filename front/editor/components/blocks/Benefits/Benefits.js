f8f9628bfbe4c7d3986d5320574d82c0db7e82c5
jonaujo89 <jonaujo89@gmail.com>
2020-03-23 00:10:52 +0100

    'Benefits 5/5'

--- front/editor/components/blocks/Benefits/Benefits.js
+++ front/editor/components/blocks/Benefits/Benefits.js
@@ -1,5 +1,6 @@
 require("./Benefits.tea");
-const {Block,Repeater,Text,Icon,BlockColor,Switch,Dialog} = require("../../internal");
+
+const {Block,Repeater,Text,Image,Icon,BlockColor,Switch,Dialog} = require("../../internal");
 
 class Benefits extends Block {
 
@@ -62,8 +63,8 @@ class Benefits extends Block {
             </div>
         </div>
     `}
-    
-    tpl_default_1() { 
+
+    tpl_default_1() {
         return window.locale_lang=='en' ? {
             'show_title': true,
             'show_title_2': false,
@@ -153,8 +154,8 @@ class Benefits extends Block {
             </div>
         </div>
     `}
-    
-    tpl_default_2() { 
+
+    tpl_default_2() {
         return window.locale_lang=='en' ? {
             'show_title': true,
             'show_title_2': false,
@@ -198,7 +199,276 @@ class Benefits extends Block {
                 }
             ]
         };
-    }    
+    }
+
+    tpl_3(val) {
+        return html`
+        <div class="container-fluid benefits benefits_3" style="background: ${val.background_color}">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        ${ val.show_title && html`
+                            <h1 class="title">
+                                <${Text} name="title" options=${Text.plain_text} />
+                            </h1>
+                        `}
+                        ${ val.show_title_2 && html`
+                            <div class="title_2">
+                                <${Text} name="title" options=${Text.plain_text} />
+                            </div>
+                        `}                        
+                        <div class="item_list ${val.show_name_benefit ? "" : "hide_name"}">
+                            <${Repeater} name="items">${item_val => html`
+                                <div class="row">
+                                    ${ [1,2].map((i) => html`
+                                        <div class="item col-6">                                
+                                            <${Icon} name=${'icon_'+i} />
+                                            ${ val.show_name_benefit && html`
+                                                <div class="name">
+                                                    <${Text} name=${'name_'+i} options=${Text.plain_heading} />
+                                                </div>
+                                            `}
+                                            <div class="desc">
+                                                <${Text} name=${'desc_'+i} options=${Text.plain_text} />
+                                            </div>
+                                        </div>
+                                    `)}
+                                </div>
+                            `}<//>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `}
+
+    tpl_default_3() {
+        return window.locale_lang=='en' ? {
+            'show_title': true,
+            'show_title_2': false,
+            'show_icon_arounds': true,
+            'show_name_benefit': true,
+            'background_color':'#F7F7F7',
+            'title': "The advantages of our circus",
+            'title_2': "Subtitle",
+            'items': [
+                {
+                    'icon_1': `${lp.app.options.assets_url}/ico/3.png`,
+                    'name_1': "Color TV",
+                    'desc_1': "We have a color TV RUBIN-2M in the lobby.",
+                    'icon_2': `${lp.app.options.assets_url}/ico/790.png`,
+                    'name_2': "Cozy circus tent",
+                    'desc_2': "Our tent will protect you from snow and wind during a spectacular view.",
+                },
+                {
+                    'icon_1': `${lp.app.options.assets_url}/ico/231.png`,
+                    'name_1': "Discount system",
+                    'desc_1': "Buy a ticket to the circus for six months and you can live here free of charge for another three months.",
+                    'icon_2': `${lp.app.options.assets_url}/ico/150.png`,
+                    'name_2': "Barbecue at the entrance gate",
+                    'desc_2': "Uncle Izzy cooks barbecue for visitors and aunt Sarah accompanies him.",
+                }
+            ]
+        } : {
+            'show_title': true,
+            'show_title_2': false,
+            'show_icon_arounds': true,
+            'show_name_benefit': true,
+            'background_color':'#F7F7F7',
+            'title': "Преимущества нашего цирка",
+            'title_2': "Подзаголовок о преимуществах",
+            'items': [
+                {
+                    'icon_1': `${lp.app.options.assets_url}/ico/3.png`,
+                    'name_1': "Цветной телевизор",
+                    'desc_1': "Только в нашем цирке есть практически цветной телевизор РУБИН-2М в холле.",
+                    'icon_2': `${lp.app.options.assets_url}/ico/790.png`,
+                    'name_2': "Уютный цирковой шатёр",
+                    'desc_2': "Наш шатер защитит Вас от снега и ветра во время захватывающего представления.",
+                },
+                {
+                    'icon_1': `${lp.app.options.assets_url}/ico/231.png`,
+                    'name_1': "Система скидок",
+                    'desc_1': "Купи абонемент в цирк на полгода и можешь оставаться жить ещё три месяца бесплатно.",
+                    'icon_2': `${lp.app.options.assets_url}/ico/150.png`,
+                    'name_2': "Шашлычная у входа",
+                    'desc_2': "Дядя Изя готовит шашлык для посетителей цирка. Аккомпанирует ему тетя Сара.",
+                }
+            ]
+        };
+    }
+
+    tpl_4(val) {
+        return html`
+        <div class="container-fluid benefits benefits_4" style="background: ${val.background_color}">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        ${ val.show_title && html`
+                            <h1 class="title">
+                                <${Text} name="title" options=${Text.plain_text} />
+                            </h1>
+                        `}
+                        ${ val.show_title_2 && html`
+                            <div class="title_2">
+                                <${Text} name="title" options=${Text.plain_text} />
+                            </div>
+                        `}                        
+                        <div class="item_list ${val.show_image_border ? "" : "hide_border"}">
+                            <${Repeater} name="items">${item_val => html`
+                                <div class="row">
+                                    ${ [1,2].map((i) => html`
+                                        <div class="item col-6">
+                                            <div class="image_wrap">
+                                                <${Image} name=${'image_'+i} />                                     
+                                            </div>
+                                            <div class="item_info">
+                                                <div class="name">
+                                                    <${Text} name=${'name_'+i} options=${Text.plain_text} />
+                                                </div>
+                                                <div class="desc">
+                                                    <${Text} name=${'desc_'+i} options=${Text.plain_text} />
+                                                </div>
+                                            </div>
+                                        </div>
+                                    `)}
+                                </div>
+                            `}<//>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `}
+
+    tpl_default_4() {
+        return window.locale_lang=='en' ? {
+            'show_title': true,
+            'show_title_2': false,
+            'show_image_border': true,
+            'background_color':'#FFFFFF',
+            'title': "The advantages of our circus",
+            'title_2': "Subtitle",
+            'items': [
+                {
+                    'image_1': `${lp.app.options.assets_url}/benefits/1.jpg`,
+                    'image_2': `${lp.app.options.assets_url}/benefits/2.jpg`,
+                    'name_1': "Serious approach",
+                    'name_2': "Attention to detail",
+                    'desc_1': "The seriousness of our approach could not be underestimated. We have the most serious circus. And we are serious!",
+                    'desc_2': "Only attention to details, such as the color of the door handles and the shape of the chair, makes us better.",
+                }
+            ]
+        } : {
+            'show_title': true,
+            'show_title_2': false,
+            'show_image_border': true,
+            'background_color':'#FFFFFF',
+            'title': "Преимущества нашего цирка",
+            'title_2': "Подзаголовок о преимуществах",
+            'items': [
+                {
+                    'image_1': `${lp.app.options.assets_url}/benefits/1.jpg`,
+                    'image_2': `${lp.app.options.assets_url}/benefits/2.jpg`,
+                    'name_1': "Серьёзный подход",
+                    'name_2': "Внимание к деталям",
+                    'desc_1': "Серьёзность нашего подхода нельзя недооценивать. Мы самый серьёзный цирк. И это мы серьезно!",
+                    'desc_2': "Только внимание к таким деталям как цвет ручки входной двери и форма стула билетёра делают нас лучшими.",
+                }
+            ]
+        };
+    }
+
+    tpl_5(val) {
+        return html`
+        <div class="container-fluid benefits benefits_5" style="background: ${val.background_color}">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        ${ val.show_title && html`
+                            <h1 class="title">
+                                <${Text} name="title" options=${Text.plain_text} />
+                            </h1>
+                        `}
+                        ${ val.show_title_2 && html`
+                            <div class="title_2">
+                                <${Text} name="title" options=${Text.plain_text} />
+                            </div>
+                        `}                        
+                        <div class="item_list ${val.show_image_border ? "" : "hide_border"}">
+                            <${Repeater} name="items">${item_val => html`
+                                <div class="row">
+                                    ${ [1,2,3].map((i) => html`
+                                        <div class="item col-4">
+                                            <${Image} name=${'image_'+i} />
+                                            ${ val.show_name_benefit && html`
+                                                <div class="name">
+                                                    <${Text} name=${'name_'+i} options=${Text.plain_text} />
+                                                </div>
+                                            `}
+                                            ${ val.show_desc_benefit && html`
+                                                <div class="desc">
+                                                    <${Text} name=${'desc_'+i} options=${Text.plain_text} />
+                                                </div>
+                                            `}
+                                        </div>
+                                    `)}
+                                </div>
+                            `}<//>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `}
+
+    tpl_default_5() {
+        return window.locale_lang=='en' ? {
+            'show_title': true,
+            'show_title_2': false,
+            'show_name_benefit': true,
+            'show_desc_benefit': true,
+            'show_image_border': true,
+            'background_color':'#FFFFFF',
+            'title': "The advantages of our circus",
+            'title_2': "Subtitle",
+            'items': [
+                {
+                    'image_1': `${lp.app.options.assets_url}/benefits/3.jpg`,
+                    'image_2': `${lp.app.options.assets_url}/benefits/4.jpg`,
+                    'image_3': `${lp.app.options.assets_url}/benefits/5.jpg`,
+                    'name_1': "Simplicity of passage",
+                    'name_2': "Seats",
+                    'name_3': "Support for a laugh",
+                    'desc_1': "The unique design of the circus will not let you get lost. Entry and exit are here in the one place.",
+                    'desc_2': "All seats are numbered. There will not any troubles with your place.",
+                    'desc_3': "Smiling dog is always here! He is always ready to lick you to death.",
+                }
+            ]
+        } : {
+            'show_title': true,
+            'show_title_2': false,
+            'show_name_benefit': true,
+            'show_desc_benefit': true,
+            'show_image_border': true,
+            'background_color':'#FFFFFF',
+            'title': "Преимущества нашего цирка",
+            'title_2': "Подзаголовок о преимуществах",
+            'items': [
+                {
+                    'image_1': `${lp.app.options.assets_url}/benefits/3.jpg`,
+                    'image_2': `${lp.app.options.assets_url}/benefits/4.jpg`,
+                    'image_3': `${lp.app.options.assets_url}/benefits/5.jpg`,
+                    'name_1': "Простота прохода",
+                    'name_2': "Места по номерам",
+                    'name_3': "Поддержка смехом",
+                    'desc_1': "Уникальная конструкция цирка не даст Вам заблудиться. Вход и выход у нас в одном месте.",
+                    'desc_2': "Все места пронумерованы черным маркером. Проблем с поиском своего места не будет.",
+                    'desc_3': "В зале всегда находится дежурный пёс-смехун, который всегда готов зализать Вас до смерти.",
+                }
+            ]
+        };
+    }
 }
 
 Block.register('Benefits',exports = Benefits);
