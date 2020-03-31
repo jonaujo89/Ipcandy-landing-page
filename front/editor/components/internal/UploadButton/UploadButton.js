a6a926f079c3bef06488338bccdd0aba8c7cda10
jonaujo89 <jonaujo89@gmail.com>
2020-03-31 15:46:52 +0200

    'FIX: uploadButton'

--- front/editor/components/internal/UploadButton/UploadButton.js
+++ front/editor/components/internal/UploadButton/UploadButton.js
@@ -1,55 +1,56 @@
 const {Editable} = require("../Editable/Editable");
 
-const UploadButton = Editable((props)=>{
+const UploadButton = Editable(class extends preact.Component{
+    render(props) {
+        const doUpload = (e) => {
+            e.preventDefault();
 
-    const doUpload = (e) => {
-        e.preventDefault();
+            if (!this.uploader) {
+                this.uploader = document.createElement("input");
+                this.uploader.type = "file";
+                this.uploader.accept = ".jpg,.jpeg,.png,.gif";
+                this.uploader.style.position = "fixed";
+                this.uploader.style.top = 0;
+                this.uploader.style.left = 0;
+                this.uploader.style.width = 0;
+                this.uploader.style.zIndex = 10000;
+                this.uploader.style.display = "none";
+                document.body.appendChild(this.uploader);
 
-        if (!this.uploader) {
-            this.uploader = document.createElement("input");
-            this.uploader.type = "file";
-            this.uploader.accept = ".jpg,.jpeg,.png,.gif";
-            this.uploader.style.position = "fixed";
-            this.uploader.style.top = 0;
-            this.uploader.style.left = 0;
-            this.uploader.style.width = 0;
-            this.uploader.style.zIndex = 10000;
-            this.uploader.style.display = "none";
-            document.body.appendChild(this.uploader);
+                this.uploader.addEventListener("change",()=>{
+                    var data = new FormData();
+                    [...this.uploader.files].forEach((file,i) => {                
+                        data.append('file-'+i, file);
+                    });
+                    data.append('name',this.props.uploadDir);
+                    data.append('iconWidth',this.props.iconWidth);
+                    data.append('iconHeight',this.props.iconHeight);
 
-            this.uploader.addEventListener("change",()=>{
-                var data = new FormData();
-                [...this.uploader.files].forEach((file,i) => {                
-                    data.append('file-'+i, file);
-                });
-                data.append('name',props.uploadDir);
-                data.append('iconWidth',props.iconWidth);
-                data.append('iconHeight',props.iconHeight);
-
-                lp.app.request("upload",data,(data)=>{
-                    data = JSON.parse(data);
-                    if (data && data.length) {
-                        if (data[0].error) {
-                            alert(data[0].error);
-                        } else {
-                            var sub_url = data[0].url;
-                            if (sub_url[0]=="/") sub_url = sub_url.substring(1);
-                            props.onChange(sub_url);
+                    lp.app.request("upload",data,(data)=>{
+                        data = JSON.parse(data);
+                        if (data && data.length) {
+                            if (data[0].error) {
+                                alert(data[0].error);
+                            } else {
+                                var sub_url = data[0].url;
+                                if (sub_url[0]=="/") sub_url = sub_url.substring(1);
+                                this.props.onChange(sub_url);
+                            }
                         }
-                    }
-                });
-            });            
+                    });
+                });            
+            }
+            
+            this.uploader.value = "";
+            this.uploader.style.display = "";
+            this.uploader.click();
+            this.uploader.style.display = "none";
         }
-        
-        this.uploader.value = "";
-        this.uploader.style.display = "";
-        this.uploader.click();
-        this.uploader.style.display = "none";
-    }
 
-    return html`
-        <button onClick=${doUpload}>${props.label}</button>
-    `;
+        return html`
+            <button onClick=${doUpload}>${props.label}</button>
+        `;
+    }
 })
 UploadButton.defaultProps = {
     iconWidth: 64,
